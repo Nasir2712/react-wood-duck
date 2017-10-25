a5435ad51e7fd26e944db93faa79491091144309
Ramu Kammagani <cambriadeveloper@Cambrias-MacBook-Pro.local>
2017-10-25 11:29:45 -0700

    Updated Global Header & Alert components

--- stories/components/Alert.js
+++ stories/components/Alert.js
@@ -27,14 +27,28 @@ const alert = withInfo(
 )(() => (
   <div>
     <Alert
-      alertClassName="info"
-      alertMessage="Info Message! Well done guys"
-      faIcon="fa-check-circle"
-    />
-
-    <Alert
       alertClassName="error"
       alertMessage="Error! This is an error message."
+      faIcon="fa-exclamation-triangle"
+    />
+    <Alert
+      alertClassName="warning"
+      alertMessage="Warning! Indicates a warning that might need attention."
+      faIcon="fa-exclamation-triangle"
+    />
+    <Alert
+      alertClassName="info"
+      alertMessage="Heads up! This alert needs your attention, but it's not super important.vertical-align: middle;vertical-align: middle;vertical-align: middle;vertical-align: middle;vertical-align: middle;vertical-align: middle;vertical-align: middle;vertical-align: middle;vertical-align: middle;"
+      faIcon="fa-info-circle"
+    />
+    <Alert
+      alertClassName="success"
+      alertMessage="Well done! You successfully completed this task."
+      faIcon="fa-check-circle"
+    />
+    <Alert
+      alertClassName="error"
+      alertMessage="Error it enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       faIcon="fa-exclamation-triangle"
     />
   </div>
